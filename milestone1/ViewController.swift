1c5864e153af624e74168fefd39b063714d5077e
Ryan Aubrey <RyanAubrey@d-172-25-249-193.dhcp.virginia.edu>
2016-04-03 20:22:48 -0400

    Initial Commit

---
+++ milestone1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  milestone1
+//
+//  Created by Ryan Aubrey on 4/3/16.
+//  Copyright © 2016 Ryan Aubrey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
